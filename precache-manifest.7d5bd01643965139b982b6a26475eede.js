5e49b91b4bee0f80c72fb9aa7d7d5ac384ddacf5
Jason Whitted <jason.whitted@clover.com>
2020-08-14 09:48:53 -0600

    Updates

---
+++ precache-manifest.7d5bd01643965139b982b6a26475eede.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c242fb9d168c1b6eb06b1fe64d23f29b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "048f930f28d99b250c35",
+    "url": "/static/css/main.4be3a9c4.chunk.css"
+  },
+  {
+    "revision": "0d9171ecf73c246b6db1",
+    "url": "/static/js/2.83bdbbff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.83bdbbff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "048f930f28d99b250c35",
+    "url": "/static/js/main.91fc754a.chunk.js"
+  },
+  {
+    "revision": "68cd382f5364db25ea5d",
+    "url": "/static/js/runtime-main.2199642b.js"
+  }
+]);
